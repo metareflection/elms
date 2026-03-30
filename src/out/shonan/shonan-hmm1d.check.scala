d5188271e494c86ed6790cc212688ede0004bcf1
Cameron Wong <cam@camdar.io>
2026-03-30 16:02:14 -0400

    actually commit hmm1d check file

---
+++ src/out/shonan/shonan-hmm1d.check.scala
@@ -0,0 +1,138 @@
+def snippet(x0: Array[Int]): Array[Int] = {
+  val x1 = new Array[Int](5)
+  val x2 = 0 until 5
+  val x4 = x2.start
+  val x5 = x2.end
+  val x12 = for (x3 <- x4 until x5) {
+    val x6 = x1(0)
+    val x7 = Array(1,1,1,1,1)(x3)
+    val x8 = x0(x3)
+    val x9 = x7 * x8
+    val x10 = x6 + x9
+    val x11 = x1(0) = x10
+    x11
+  }
+  
+  val x13 = x1(1)
+  val x14 = Array(0,0,0,0,0)(0)
+  val x15 = x0(0)
+  val x16 = x14 * x15
+  val x17 = x13 + x16
+  val x18 = x1(1) = x17
+  val x19 = x1(1)
+  val x20 = Array(0,0,0,0,0)(1)
+  val x21 = x0(1)
+  val x22 = x20 * x21
+  val x23 = x19 + x22
+  val x24 = x1(1) = x23
+  val x25 = x1(1)
+  val x26 = Array(0,0,0,0,0)(2)
+  val x27 = x0(2)
+  val x28 = x26 * x27
+  val x29 = x25 + x28
+  val x30 = x1(1) = x29
+  val x31 = x1(1)
+  val x32 = Array(0,0,0,0,0)(3)
+  val x33 = x0(3)
+  val x34 = x32 * x33
+  val x35 = x31 + x34
+  val x36 = x1(1) = x35
+  val x37 = x1(1)
+  val x38 = Array(0,0,0,0,0)(4)
+  val x39 = x0(4)
+  val x40 = x38 * x39
+  val x41 = x37 + x40
+  val x42 = x1(1) = x41
+  val x43 = x1(2)
+  val x44 = Array(0,0,1,0,0)(0)
+  val x45 = x0(0)
+  val x46 = x44 * x45
+  val x47 = x43 + x46
+  val x48 = x1(2) = x47
+  val x49 = x1(2)
+  val x50 = Array(0,0,1,0,0)(1)
+  val x51 = x0(1)
+  val x52 = x50 * x51
+  val x53 = x49 + x52
+  val x54 = x1(2) = x53
+  val x55 = x1(2)
+  val x56 = Array(0,0,1,0,0)(2)
+  val x57 = x0(2)
+  val x58 = x56 * x57
+  val x59 = x55 + x58
+  val x60 = x1(2) = x59
+  val x61 = x1(2)
+  val x62 = Array(0,0,1,0,0)(3)
+  val x63 = x0(3)
+  val x64 = x62 * x63
+  val x65 = x61 + x64
+  val x66 = x1(2) = x65
+  val x67 = x1(2)
+  val x68 = Array(0,0,1,0,0)(4)
+  val x69 = x0(4)
+  val x70 = x68 * x69
+  val x71 = x67 + x70
+  val x72 = x1(2) = x71
+  val x73 = x1(3)
+  val x74 = Array(0,0,0,0,0)(0)
+  val x75 = x0(0)
+  val x76 = x74 * x75
+  val x77 = x73 + x76
+  val x78 = x1(3) = x77
+  val x79 = x1(3)
+  val x80 = Array(0,0,0,0,0)(1)
+  val x81 = x0(1)
+  val x82 = x80 * x81
+  val x83 = x79 + x82
+  val x84 = x1(3) = x83
+  val x85 = x1(3)
+  val x86 = Array(0,0,0,0,0)(2)
+  val x87 = x0(2)
+  val x88 = x86 * x87
+  val x89 = x85 + x88
+  val x90 = x1(3) = x89
+  val x91 = x1(3)
+  val x92 = Array(0,0,0,0,0)(3)
+  val x93 = x0(3)
+  val x94 = x92 * x93
+  val x95 = x91 + x94
+  val x96 = x1(3) = x95
+  val x97 = x1(3)
+  val x98 = Array(0,0,0,0,0)(4)
+  val x99 = x0(4)
+  val x100 = x98 * x99
+  val x101 = x97 + x100
+  val x102 = x1(3) = x101
+  val x103 = x1(4)
+  val x104 = Array(0,0,1,0,1)(0)
+  val x105 = x0(0)
+  val x106 = x104 * x105
+  val x107 = x103 + x106
+  val x108 = x1(4) = x107
+  val x109 = x1(4)
+  val x110 = Array(0,0,1,0,1)(1)
+  val x111 = x0(1)
+  val x112 = x110 * x111
+  val x113 = x109 + x112
+  val x114 = x1(4) = x113
+  val x115 = x1(4)
+  val x116 = Array(0,0,1,0,1)(2)
+  val x117 = x0(2)
+  val x118 = x116 * x117
+  val x119 = x115 + x118
+  val x120 = x1(4) = x119
+  val x121 = x1(4)
+  val x122 = Array(0,0,1,0,1)(3)
+  val x123 = x0(3)
+  val x124 = x122 * x123
+  val x125 = x121 + x124
+  val x126 = x1(4) = x125
+  val x127 = x1(4)
+  val x128 = Array(0,0,1,0,1)(4)
+  val x129 = x0(4)
+  val x130 = x128 * x129
+  val x131 = x127 + x130
+  val x132 = x1(4) = x131
+  x1
+}
+
