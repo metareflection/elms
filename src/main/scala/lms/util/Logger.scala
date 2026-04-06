f890c092fb02818986cffaf90700b14c3d7fbd8f
Cameron Wong <cam@camdar.io>
2026-04-05 22:50:42 -0400

    add debug logger

--- src/main/scala/lms/util/Logger.scala
+++ src/main/scala/lms/util/Logger.scala
@@ -4,17 +4,32 @@ trait Logger {
   def info(msg: String)(using pos: SourceContext): Unit
   def warning(msg: String)(using pos: SourceContext): Unit
   def error(msg: String)(using pos: SourceContext): Unit
+  def debug(msg: String)(using pos: SourceContext): Unit
 }
 
 object Logger {
+  def toStderr(prefix: String, msg: String)(using pos: SourceContext): Unit =
+    System.err.println(s"[${pos.render}] $prefix: $msg")
+
+  def toStdout(prefix: String, msg: String)(using pos: SourceContext): Unit =
+    System.out.println(s"[${pos.render}] $prefix: $msg")
+
+  def debugStderr(msg: String)(using SourceContext): Unit =    toStderr("Debug", msg)
+  def infoStderr(msg: String)(using SourceContext): Unit =    toStderr("Info", msg)
+  def warnStderr(msg: String)(using SourceContext): Unit =    toStderr("Warning", msg)
+  def errorStderr(msg: String)(using SourceContext): Unit =    toStderr("Error", msg)
+
   val default = new Logger {
-    def info(msg: String)(using pos: SourceContext): Unit = System.err
-      .println(s"[$pos.render]: $msg")
+    def debug(msg: String)(using pos: SourceContext): Unit = {}
+    def info(msg: String)(using pos: SourceContext): Unit = {}
+    def warning(msg: String)(using pos: SourceContext): Unit = warnStderr(msg)
+    def error(msg: String)(using pos: SourceContext): Unit = errorStderr(msg)
+  }
 
-    def warning(msg: String)(using pos: SourceContext): Unit = System.err
-      .println(s"[$pos.render]: $msg")
-
-    def error(msg: String)(using pos: SourceContext): Unit = System.err
-      .println(s"[$pos.render]: $msg")
+  val debug = new Logger {
+    def debug(msg: String)(using pos: SourceContext): Unit = debugStderr(msg)
+    def info(msg: String)(using pos: SourceContext): Unit = infoStderr(msg)
+    def warning(msg: String)(using pos: SourceContext): Unit = warnStderr(msg)
+    def error(msg: String)(using pos: SourceContext): Unit = errorStderr(msg)
   }
 }
